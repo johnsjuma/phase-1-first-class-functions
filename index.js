4c535319f7298c1cf24e0671734bc28f9751bc3e
jerryjohns <jerry.johns@student.moringaschool.com>
2022-06-08 10:51:15 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+function receivesAFunction(callback) {
+    callback();
+}
+function returnsANamedFunction() {
+    const namedFn = () => console.log("this is a named function");
+    return namedFn;}
+    function returnsAnAnonymousFunction() {
+        return () => console.log("this is an anonymous function");
+    }
+    
