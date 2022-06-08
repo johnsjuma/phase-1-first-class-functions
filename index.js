5fbb8068c96118dfac57a21ab265dfc542bb2355
jerryjohns <jerry.johns@student.moringaschool.com>
2022-06-08 10:49:15 +0300

    commit tree 9a9038db7d5ec252871ccbecc1fda1f19812437f

---
+++ index.js
@@ -0,0 +1,9 @@
+function receivesAFunction(callback) {
+    callback();
+}
+function returnsANamedFunction() {
+    const namedFn = () => console.log("this is a named function");
+    return namedFn;}
+    function returnsAnAnonymousFunction() {
+        return () => console.log("this is an anonymous function");
+    }
